1cce4d0317a411ed996bc861c0321d88445360ba
Lyric Payton <ljpayton8@gmail.com>
2018-10-07 23:07:46 -0700

    first commit

---
+++ Milestone2/Driver.java
@@ -0,0 +1,20 @@
+/** Program: Driver
+* File: Driver.java 
+* Summary: Main Driver method that displays the attributes.
+* Author: Lyric Payton
+* Date: October 7, 2018
+**/
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// default constructor 
+		Player p = new Player();
+		// non default constructor 
+		Player p2 = new Player();
+		// display
+		System.out.println(p2);
+		System.out.println(p);
+	}
+
+}
